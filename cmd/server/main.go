4573f4b29aef5571ff9c1af4fa185ea8e2fe1caf
Gustavo Arendt <gustavo.arendt@outlook.com>
2023-12-20 15:36:41 -0300

    feat: general configs

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gustavoarendt/jobtracker/configs"
+	"github.com/gustavoarendt/jobtracker/internal/database"
+	"github.com/gustavoarendt/jobtracker/internal/routes"
+)
+
+func main() {
+	config, err := configs.Configure("C:\\Users\\Gus\\projects\\go\\jobtracker\\cmd\\server")
+	if err != nil {
+		panic(err)
+	}
+	database.DbConnection(config)
+	routes.HandleRequest(config)
+}
---
+++ configs/config.go
@@ -0,0 +1,39 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var configuration *Config
+
+type Config struct {
+	DBDriver     string `mapstructure:"DB_DRIVER"`
+	DBHost       string `mapstructure:"DB_HOST"`
+	DBPort       string `mapstructure:"DB_PORT"`
+	DBUser       string `mapstructure:"DB_USER"`
+	DBPassword   string `mapstructure:"DB_PASSWORD"`
+	DBName       string `mapstructure:"DB_NAME"`
+	DBTimezone   string `mapstructure:"DB_TIMEZONE"`
+	ServerPort   string `mapstructure:"SERVER_PORT"`
+	JWTSecret    string `mapstructure:"SERVER_PORT"`
+	JWTExpiresIn string `mapstructure:"JWT_EXPIRES_IN"`
+	TokenAuth    *jwtauth.JWTAuth
+}
+
+func Configure(path string) (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigFile(".env")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	err = viper.Unmarshal(&configuration)
+	if err != nil {
+		return nil, err
+	}
+	configuration.TokenAuth = jwtauth.New("HS256", []byte(configuration.JWTSecret), nil)
+	return configuration, nil
+}
